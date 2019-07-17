24012cd9103328d2e981ae70cfdc0f15715efafd
Rabie TAWIL <rabietawil@gmail.com>
2019-07-17 13:02:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+class Driver{
+  constructor(name,startDate){
+    this.name = name,
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year){
+    let yOfExperience = year - this.startDate.getFullYear(); 
+    return yOfExperience;
+  }
+}
+
+class Route{
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation = beginningLocation,
+    this.endingLocation = endingLocation
+  }
+  
+}
 
